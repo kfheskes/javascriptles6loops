8502ab049e910ed634eb45048db1b3016360e32e
kfheskes <kayarneheskes@hotmail.com>
2023-03-21 17:03:01 +0100

    uitwerkingen21031702

--- loops.js
+++ loops.js
@@ -11,9 +11,8 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
-
-
-
+for (let i = 0; i < 5; i++) {
+    console.log("*".repeat(i+1));}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
@@ -25,9 +24,10 @@
 // klaar!
 // ==========================================
 
-
-
-
+for (let i = 0; i < 5; i++) {
+    if (i === 4)
+{ console.log("klaar!")}
+else { console.log("loop..."); }}
 
 // ==========================================
 // Opdracht 3: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -45,8 +45,13 @@
 // >> 9
 // ==========================================
 
-
-
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i);}
+    if (i >= 3 && i < 6) {
+        console.log(">" + i) }
+    if (i >= 6) {
+        console.log(">>" + i) }}
 
 
 // ==========================================
@@ -87,5 +92,15 @@
 // etc.
 // ==========================================
 
+for (let i = 0; i < 101; i++) {
+    if (i%3 === 0) {
+        console.log('Fizz')
+    } else if (i%5 === 0) {
+        console.log('Buzz')
+    }
+    else {
+        console.log(i)
+    }
 
+}
 
